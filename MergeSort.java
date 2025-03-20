51a918aff4b14fdc4ad91bb9263154d886128aca
guttulaswathi <swathiguttula639@gmail.com>
2025-03-20 06:33:34 +0530

    MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -1,49 +1,63 @@
-import java.util.Scanner;
-public class MergeSort
-{
-	private static int[] b;
-	public static void mergeSort(int[] a,int low,int high)
-	{
-		if(low<high)
-		{
-			int mid=(low+high)/2;
-			mergeSort(a,low,mid);
-			mergeSort(a,mid+1,high);
-			merge(a,low,mid,high);
-		}
-	}
-	public static void merge(int[] a,int low,int mid,int high)
-	{
-		int i=low;
-		int j=mid+1;
-		int k=low;
-		while(i<=mid && j<=high)
-			if(a[i]<=a[j])
-				b[k++]=a[i++];
-			else
-				b[k++]=a[j++];
-		while(i<=mid)
-			b[k++]=a[i++];
-		while(j<=high)
-			b[k++]=a[j++];
-		for(int h=low;h<=high;h++)
-			a[h]=b[h];
-	}
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter the number of elements in the array:");
-		int n=sc.nextInt();
-		int[] array=new int[n];
-		b=new int[n];
-		System.out.println("enter the elements of the array");
-		for(int i=0;i<n;i++)
-		{
-			array[i]=sc.nextInt();
-		}
-		mergeSort(array,0,array.length-1);
-		System.out.println("sorted Array:");
-		for(int num:array)
-			System.out.println(num+" ");
-	}
+import java.util.*;
+
+public class MergeSort {
+    public static void mergesort(int arr[], int low, int high) {
+        if (low >= high) { // base condition
+            return;
+        }
+
+        int mid = low + (high - low) / 2; // Correct mid calculation
+        mergesort(arr, low, mid);
+        mergesort(arr, mid + 1, high);
+        merge(arr, low, high, mid);
+    }
+
+    public static void merge(int arr[], int low, int high, int mid) {
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+
+        int[] leftarray = new int[n1];
+        int[] rightarray = new int[n2];
+
+        System.arraycopy(arr, low, leftarray, 0, n1);
+        System.arraycopy(arr, mid + 1, rightarray, 0, n2);
+
+        int i = 0, j = 0, k = low;
+        while (i < n1 && j < n2) {
+            if (leftarray[i] <= rightarray[j]) {
+                arr[k++] = leftarray[i++];
+            } 
+            else
+            {
+                arr[k++] = rightarray[j++];
+            }
+        }
+
+        // Copy remaining elements if any
+        while (i < n1) {
+            arr[k++] = leftarray[i++];
+        }
+        while (j < n2) {
+            arr[k++] = rightarray[j++];
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter number of elements:");
+        int n = s.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("enter elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        mergesort(arr, 0, arr.length - 1);
+        System.out.println("sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " "); 
+        }
+    }
 }
+
